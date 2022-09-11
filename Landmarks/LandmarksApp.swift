b2f1238a2997cfba62a9700db6b244572bc8d4b1
brightbong92 <wkdgusqhd080@naver.com>
2022-09-11 18:29:40 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by bong on 2022/09/11.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

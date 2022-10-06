a9348a7443a187f106dd9451eacd14876287b740
brightbong92 <wkdgusqhd080@naver.com>
2022-10-07 00:23:23 +0900

    update list

--- Views/LandmarkList.swift
+++ Views/LandmarkList.swift
@@ -12,15 +12,25 @@ struct LandmarkList: View {
 //        List(landmarks) {landmark in
 //            LandmarkRow(landmark: landmark)
 //        }
-        
-        List(landmarks, id: \.id) {landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationView {
+            List(landmarks, id: \.id) {landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label : {
+                    LandmarkRow(landmark: landmark)
+                }   
+            }
+            .navigationTitle("Landmarks")
         }
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+                 LandmarkList()
+                     .previewDevice(PreviewDevice(rawValue: deviceName))
+                     .previewDisplayName(deviceName)
+        }
     }
 }

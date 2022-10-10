686663898b3db10b4d82dde0257e6bd325dd605f
brightbong92 <wkdgusqhd080@naver.com>
2022-10-10 20:26:01 +0900

    update favoriteButton

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -8,8 +8,7 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
             ContentView()
-        }
+            .environmentObject(ModelData())
     }
 }
